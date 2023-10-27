a06dbd4e076d94d10106dc5f0ace053808bf20a0
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2023-10-27 21:02:55 +0800

    v231027_2102

--- pkg/tool/sha256.go
+++ pkg/tool/sha256.go
@@ -3,6 +3,7 @@ package tool
 import (
     "fmt"
     "time"
+    "encoding/hex"
     "path/filepath"
     "crypto/sha256"
 )
@@ -11,11 +12,10 @@ func SHA256EncodeTime(input string) string {
     hasher := sha256.New()
     hasher.Write([]byte(input + time.Now().Format("2006-01-02 15:04:05")))
 
-    return fmt.Sprintf("%x", hasher.Sum(nil))
+    return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func SHA256FilePath(base, input string) string {
-    fName := SHA256EncodeTime(input) + filepath.Ext(input)
-
-    return filepath.Join(base, fName)
+    fmt.Println(filepath.Join(base, SHA256EncodeTime(input) + filepath.Ext(input)))
+    return filepath.Join(base, SHA256EncodeTime(input) + filepath.Ext(input))
 }
