932e5198fb846746e820c4761d6628ba74a029a0
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2023-11-30 12:30:40 +0800

    v231130_1230

--- pkg/api/api_login.go
+++ pkg/api/api_login.go
@@ -24,9 +24,15 @@ type LoginReponse struct {
 func RouterLogin(db SmartHubDatabase.SmartHubDB) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
-    	w.Header().Set("Access-Control-Allow-Methods", "POST")
+    	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+    	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		switch r.Method {
+            case "OPTIONS":
+    			w.WriteHeader(http.StatusOK)
+
+    			return
+
 			case "POST" :
                 var Req LoginRequest
 
