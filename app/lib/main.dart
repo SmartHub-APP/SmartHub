f3f01295936be92e704dcf0c7461fc5348ce01b7
Connection || Peter <69660530+ConnectionOuOb@users.noreply.github.com>
2024-01-22 15:05:36 +0800

    v240122_1505

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,6 +10,8 @@ import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
+import 'tabs/setting.dart';
+
 // ##### Initialize
 bool autoLogin = false;
 FluroRouter router = FluroRouter();
@@ -112,16 +114,16 @@ class _RoutePageState extends State<RoutePage> with SingleTickerProviderStateMix
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.black,
-            leading: Center(child: manager.icon),
             leadingWidth: 12.w,
+            leading: Center(child: manager.icon),
+            titleSpacing: -50,
             title: TabBar(
               isScrollable: true,
               controller: _tabController,
-              indicatorColor: Colors.white,
+              indicator: const UnderlineTabIndicator(),
               onTap: (tabIndex) {
                 router.navigateTo(context, showTabs[tabIndex].route, transition: TransitionType.none);
               },
-              indicator: const UnderlineTabIndicator(),
               tabs: [
                 // 1. Dashboard
                 if (manager.tabPermission.dashboard > 0) Tab(text: context.tr('dashboard_tabName')),
@@ -140,7 +142,7 @@ class _RoutePageState extends State<RoutePage> with SingleTickerProviderStateMix
                 icon: const Icon(Icons.settings),
                 tooltip: context.tr('setting'),
                 onPressed: () {
-                  settingDialog();
+                  settingDialog(context);
                 },
               ),
               SizedBox(width: 1.w),
@@ -160,118 +162,6 @@ class _RoutePageState extends State<RoutePage> with SingleTickerProviderStateMix
             children: showTabs.map((e) => e.content as Widget).toList(),
           ),
         );
-      },
-    );
-  }
-
-  settingDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(builder: (context, setSettingState) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(borderRadius: uiStyle.roundCorner),
-            backgroundColor: Colors.white,
-            titlePadding: EdgeInsets.zero,
-            title: Container(
-              alignment: Alignment.centerLeft,
-              width: 40.w,
-              height: 7.h,
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-              ),
-              child: Center(child: text2(context.tr('setting'), color: Colors.white)),
-            ),
-            content: Container(
-              alignment: Alignment.topCenter,
-              width: 60.w,
-              height: 80.h,
-              decoration: BoxDecoration(borderRadius: uiStyle.roundCorner),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ExpansionTile(
-                      title: text3(context.tr('settingLanguage'), isBold: true),
-                      children: [
-                        SizedBox(height: 2.h),
-                        Wrap(
-                          spacing: 10,
-                          runSpacing: 5,
-                          alignment: WrapAlignment.center,
-                          children: ini.languages.map((lang) {
-                            return InkWell(
-                              onTap: () {
-                                context.setLocale(lang.ref);
-                              },
-                              child: Container(
-                                width: 150,
-                                height: 45,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  border: Border.all(color: Colors.grey),
-                                  borderRadius: uiStyle.roundCorner2,
-                                ),
-                                child: text3(lang.langName),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        SizedBox(height: 2.h),
-                      ],
-                    ),
-                    ExpansionTile(
-                      title: text3(context.tr('settingUserCreate'), isBold: true),
-                      children: [
-                        SizedBox(height: 2.h),
-                        InkWell(
-                          onTap: () {},
-                          child: Container(
-                            width: 150,
-                            height: 45,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(color: Colors.grey),
-                              borderRadius: uiStyle.roundCorner2,
-                            ),
-                            child: text3(context.tr("settingUserCreate")),
-                          ),
-                        ),
-                        SizedBox(height: 1.h),
-                        //ListView(),
-                        SizedBox(height: 2.h),
-                      ],
-                    ),
-                    ExpansionTile(
-                      title: text3(context.tr('settingRoleSetting'), isBold: true),
-                      children: [
-                        SizedBox(height: 2.h),
-                        SizedBox(height: 2.h),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            actionsAlignment: MainAxisAlignment.center,
-            actionsPadding: EdgeInsets.only(bottom: 2.h),
-            actions: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.symmetric(horizontal: 3.w, vertical: 2.h)),
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: uiStyle.roundCorner)),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: text3(context.tr('save'), color: Colors.white),
-              ),
-            ],
-          );
-        });
       },
     );
   }
