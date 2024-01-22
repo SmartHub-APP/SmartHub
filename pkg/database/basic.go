b12f3c7bcacf0919d45091d2c364bd2c2ad931ea
Connection Linas <d123456789000@gmail.com>
2024-01-22 14:11:23 +0800

    v240122_1408

--- pkg/database/basic.go
+++ pkg/database/basic.go
@@ -1,10 +1,11 @@
 package database
 
 import (
-	"fmt"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	SmartHubTool "SmartHub/pkg/tool"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type SmartHubDB struct {
@@ -12,15 +13,17 @@ type SmartHubDB struct {
 }
 
 const (
-    maxOpenConns int = 10
-    maxIdleConns int = 10
+	maxOpenConns int = 10
+	maxIdleConns int = 10
 )
 
 func (DB *SmartHubDB) Connection(cfg SmartHubTool.SettingConfig) (bool, string) {
 	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.DB_User, cfg.DB_Password, cfg.DB_Address, cfg.DB_Port, cfg.DB_Name)
 	DB.ctl, _ = sql.Open("mysql", uri)
 
-	if err := DB.ctl.Ping(); err != nil { return false, err.Error() }
+	if err := DB.ctl.Ping(); err != nil {
+		return false, err.Error()
+	}
 
 	DB.ctl.SetMaxOpenConns(maxOpenConns)
 	DB.ctl.SetMaxIdleConns(maxIdleConns)
@@ -30,19 +33,29 @@ func (DB *SmartHubDB) Connection(cfg SmartHubTool.SettingConfig) (bool, string) 
 
 func (DB *SmartHubDB) CheckTable(cfg SmartHubTool.SettingConfig) (bool, string) {
 	_, isExist := DB.ctl.Query("select * from File;")
-    if isExist != nil { return false, isExist.Error() }
+	if isExist != nil {
+		return false, isExist.Error()
+	}
 
-	_, isExist  = DB.ctl.Query("select * from Role;")
-    if isExist != nil { return false, isExist.Error() }
+	_, isExist = DB.ctl.Query("select * from Role;")
+	if isExist != nil {
+		return false, isExist.Error()
+	}
 
-	_, isExist  = DB.ctl.Query("select * from Member;")
-    if isExist != nil { return false, isExist.Error() }
+	_, isExist = DB.ctl.Query("select * from Member;")
+	if isExist != nil {
+		return false, isExist.Error()
+	}
 
-	_, isExist  = DB.ctl.Query("select * from Transaction;")
-    if isExist != nil { return false, isExist.Error() }
+	_, isExist = DB.ctl.Query("select * from Transaction;")
+	if isExist != nil {
+		return false, isExist.Error()
+	}
 
-	_, isExist  = DB.ctl.Query("select * from Appointment;")
-    if isExist != nil { return false, isExist.Error() }
+	_, isExist = DB.ctl.Query("select * from Appointment;")
+	if isExist != nil {
+		return false, isExist.Error()
+	}
 
 	return true, "success"
 }
